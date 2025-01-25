ba9bcaf370ec470a448e421b8b4b357e355da4bc
Tom Lever <Thomas.Lever.Business@Gmail.com>
2025-01-24 20:59:19 -0500

    Allow stacking channels from Henry and GEE

--- stack_channels.py
+++ stack_channels.py
@@ -5,33 +5,30 @@ import os
 
 
 def load_channel(image_path):
-    try:
-        with Image.open(image_path) as img:
-            if img.mode == 'RGB':
-                r, _, _ = img.split()
-                gray_img = r
-                array = np.array(gray_img)
-            elif img.mode == 'RGBA':
-                r, _, _, _ = img.split()
-                gray_img = r
-                array = np.array(gray_img)
-            elif img.mode == 'F':
-                array_float = np.array(img, dtype = np.float32)
-                max_val = array_float.max()
-                if max_val > 1.0:
-                    scaled_array = (array_float / max_val) * 255.0
-                else:
-                    scaled_array = array_float * 255.0
-                scaled_array = np.clip(scaled_array, 0, 255).astype(np.uint8)
-                return scaled_array
-            elif img.mode == 'L':
-                array = np.array(img)
+    with Image.open(image_path) as img:
+        if img.mode == 'RGB':
+            r, _, _ = img.split()
+            gray_img = r
+            array = np.array(gray_img)
+        elif img.mode == 'RGBA':
+            r, _, _, _ = img.split()
+            gray_img = r
+            array = np.array(gray_img)
+        elif img.mode == 'F':
+            array_float = np.array(img, dtype = np.float32)
+            max_val = array_float.max()
+            if max_val > 1.0:
+                scaled_array = (array_float / max_val) * 255.0
             else:
-                gray_img = img.convert('L')
-                array = np.array(gray_img)
-            return array
-    except Exception as e:
-        return None
+                scaled_array = array_float * 255.0
+            scaled_array = np.clip(scaled_array, 0, 255).astype(np.uint8)
+            return scaled_array
+        elif img.mode == 'L':
+            array = np.array(img)
+        else:
+            gray_img = img.convert('L')
+            array = np.array(gray_img)
+        return array
 
 
 def verify_dimensions(*arrays):
@@ -42,27 +39,39 @@ def verify_dimensions(*arrays):
 
 if __name__ == "__main__":
 
-    for index_of_image in range(0, 105 + 1):
+    path_to_directory_of_channels_to_stack = r".\data\output_train_FINETUNING"
+    #path_to_directory_of_channels_to_stack = r".\data\channels"
 
-        base_path = 'data/output_train_FINETUNING/'
-        red_path = os.path.join(base_path, f'r_{index_of_image}.png')
-        green_path = os.path.join(base_path, f'g_{index_of_image}.png')
-        blue_path = os.path.join(base_path, f'b_{index_of_image}.png')
-        nir_path = os.path.join(base_path, f'nir_{index_of_image}.png')
+    if path_to_directory_of_channels_to_stack == r".\data\output_train_FINETUNING":
+        prefix_for_channel_N = "nir_"
+        suffix_of_last_channel = 105
+        extension = ".png"
+    if path_to_directory_of_channels_to_stack == r".\data\channels":
+        prefix_for_channel_N = "n_"
+        suffix_of_last_channel = 10
+        extension = ".tif"
+    
 
-        red_channel = load_channel(red_path)
-        green_channel = load_channel(green_path)
-        blue_channel = load_channel(blue_path)
-        nir_channel = load_channel(nir_path)
+    for index_of_image in range(0, suffix_of_last_channel + 1):
+        red_path = os.path.join(path_to_directory_of_channels_to_stack, f'r_{index_of_image}' + extension)
+        green_path = os.path.join(path_to_directory_of_channels_to_stack, f'g_{index_of_image}' + extension)
+        blue_path = os.path.join(path_to_directory_of_channels_to_stack, f'b_{index_of_image}' + extension)
+        nir_path = os.path.join(path_to_directory_of_channels_to_stack, f'{prefix_for_channel_N}{index_of_image}' + extension)
 
-        if (red_channel is None) or (green_channel is None) or (blue_channel is None) or (nir_channel is None):
-            raise ValueError("One or more channels failed to load.")
+        try:
+            red_channel = load_channel(red_path)
+            green_channel = load_channel(green_path)
+            blue_channel = load_channel(blue_path)
+            nir_channel = load_channel(nir_path)
+        except Exception as e:
+            print(e)
+            continue
 
         verify_dimensions(red_channel, green_channel, blue_channel, nir_channel)
 
         stacked_array = np.stack([red_channel, green_channel, blue_channel, nir_channel], axis = 0)
 
-        output_tif_path = os.path.join(base_path, f'image_{index_of_image}.tif')
+        output_tif_path = os.path.join(path_to_directory_of_channels_to_stack, f'image_{index_of_image}.tif')
         tifffile.imwrite(
             output_tif_path,
             stacked_array,
