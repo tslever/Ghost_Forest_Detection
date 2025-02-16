06f9122395d2fba5fcbc4d124d0ddba716f4f6dc
Tom Lever <Thomas.Lever.Business@Gmail.com>
2025-02-16 00:02:05 -0500

    Create plot_coordinate_pairs.py

---
+++ plot_coordinate_pairs.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("output_excerpt.csv")
+
+# Plotting coordinate pairs from columns "X" and "Y"
+plt.figure(figsize=(8, 6))
+plt.scatter(df['X'], df['Y'], color='blue', marker='.')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Scatter Plot of Coordinates')
+plt.grid(True)
+plt.show()
