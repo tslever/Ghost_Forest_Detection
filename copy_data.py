a8dd53876b221ea638ed8e8de53f311dd19f48ef
Tom Lever <thomas.lever.business@gmail.com>
2025-02-22 20:44:55 -0500

    Copy testing data

--- copy_data.py
+++ copy_data.py
@@ -6,18 +6,22 @@ import shutil
 #source_folder = '../urban-tree-detection-data/images_based_on_chopped_many_training_images'
 #source_folder = '../urban-tree-detection-data/images_based_on_chopped_initial_validation_images'
 #source_folder = '../urban-tree-detection-data/images_based_on_chopped_many_validation_images'
+#source_folder = '../urban-tree-detection-data/images_based_on_chopped_testing_images'
 #source_folder = "../urban-tree-detection-data/csv_files_based_on_chopped_initial_training_tables_of_centroids_of_trees"
 #source_folder = "../urban-tree-detection-data/csv_files_based_on_chopped_many_training_tables_of_centroids_of_trees"
 #source_folder = "../urban-tree-detection-data/csv_files_based_on_chopped_initial_validation_tables_of_centroids_of_trees"
-source_folder = "../urban-tree-detection-data/csv_files_based_on_chopped_many_validation_tables_of_centroids_of_trees"
+#source_folder = "../urban-tree-detection-data/csv_files_based_on_chopped_many_validation_tables_of_centroids_of_trees"
+source_folder = "../urban-tree-detection-data/csv_files_based_on_chopped_testing_tables_of_centroids_of_trees"
 
 #listing_path = '../urban-tree-detection-data/train.txt'
 #listing_path = '../urban-tree-detection-data/val.txt'
+#listing_path = '../urban-tree-detection-data/test.txt'
 
 #destination_folder = '../urban-tree-detection-data/images'
 destination_folder = "../urban-tree-detection-data/csv"
 
 # Create the listing if it does not exist
+# TODO: Comment out when copying CSV files.
 #if not os.path.exists(listing_path):
 #    with open(listing_path, 'w') as f:
 #        pass  # File created
@@ -39,6 +43,7 @@ for file_name in os.listdir(source_folder):
         # Print the file name
         print(file_name)
         
+        # TODO: Comment everything after this out when copying CSV files.
         # Remove the file extension before appending
         #name_without_ext, _ = os.path.splitext(file_name)
         
